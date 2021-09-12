206bc95dccb603c9960120b340cc5081c78a194f
Eron <erooonmahmuti@gmail.com>
2021-09-13 00:56:27 +0200

    add images and style

--- functions.php
+++ functions.php
@@ -2,12 +2,27 @@
 
 function blog_files(){
     wp_enqueue_style('blog_styles', get_stylesheet_uri());
+    
+    wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    
+    wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=yourkeygoeshere', NULL, '1.0', true);
+  
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
+    wp_enqueue_style('our-main-styles-vendor', get_theme_file_uri('/build/index.css'));
+    wp_enqueue_style('our-main-styles', get_theme_file_uri('/build/style-index.css'));
+  
+    wp_localize_script('main-university-js', 'universityData', array(
+      'root_url' => get_site_url(),
+      'nonce' => wp_create_nonce('wp_rest')
+    ));
 }
 
 add_action('wp_enqueue_scripts', 'blog_files');
 
 //get down with this theme as soon as posible
-
+// function university_files() {
+//   }
 
 ?>
 
--- page.php
+++ page.php
@@ -3,14 +3,42 @@
 <?php
     get_header();
 
+    
     while(have_posts()){
         the_post(); ?>
-        <h1>This is a page </h1>
-        <h2> <?php the_title(); ?> </h2>
-        <hr>
-        <p> <?php the_content(); ?> </p>
-        <?php 
-}
+
+<div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>)"></div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"> <?php the_title(); ?> </h1>
+        <div class="page-banner__intro">
+          <p> <?php echo "dont forget to replace me bruh" ?> </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="container container--narrow page-section">
+      <div class="metabox metabox--position-up metabox--with-home-link">
+        <p>
+          <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
+        </p>
+      </div>
+
+      <!-- <div class="page-links">
+        <h2 class="page-links__title"><a href="#">About Us</a></h2>
+        <ul class="min-list">
+          <li class="current_page_item"><a href="#">Our History</a></li>
+          <li><a href="#">Our Goals</a></li>
+        </ul>
+      </div> -->
+
+      <div class="generic-content">
+          <?php  the_content(); ?>
+      </div>
+    </div>
+
+    
+        <?php }
 
 get_footer();
 ?>
