e1bfa95ce64f30b508126cc933790b6f3abbf3d7
Eron <erooonmahmuti@gmail.com>
2021-09-13 02:18:24 +0200

    header update

--- header.php
+++ header.php
@@ -14,18 +14,12 @@
         <div class="site-header__menu group">
           <nav class="main-navigation">
             <ul>
-              <li><a href="#">About Us</a></li>
+              <li><a href=" <?php echo site_url('/about-us') ?> ">About Us</a></li>
               <li><a href="#">Programs</a></li>
               <li><a href="#">Events</a></li>
-              <li><a href="#">Campuses</a></li>
               <li><a href="#">Blog</a></li>
             </ul>
           </nav>
-          <div class="site-header__util">
-            <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
-            <a href="#" class="btn btn--small btn--dark-orange float-left">Sign Up</a>
-            <span class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
-          </div>
         </div>
       </div>
     </header>
